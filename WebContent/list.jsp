58314a7444f335d8b989fc9f71916981e9dc86bb
Jo-miyeon <my0v0@naver.com>
2021-01-10 13:26:22 +0900

    jsp연습

--- WebContent/list.jsp
+++ WebContent/list.jsp
@@ -1,5 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ page import="java.util.ArrayList" %>
+<%@ page import="board.article.ArticleDao" %>
+<%@ page import="board.article.Article" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +10,10 @@
 <title>Insert title here</title>
 </head>
 <body>
+<h1>게시물목록.</h1>
+<%
+	ArrayList<Article> articles = (ArrayList<Article>)request.getAttribute("myData");  
+%>
 <table border="1">
 <tr>
 		<td>번호</td>
@@ -31,5 +38,6 @@
 		System.out.println("aa");
 	}%>
 </table>
+<a href=http://localhost:8080/web-exam1/TestServlet">링크1</a>
 </body>
 </html>
---
+++ src/test/Controller.java
@@ -0,0 +1,43 @@
+package test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import board.article.Article;
+import board.article.ArticleDao;
+
+@WebServlet("/Controller")//클래스명과 서블렛명은 달라도된다.
+public class Controller extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8"); //문자 인코딩 utf-8로 설정
+		ArticleDao dao = new ArticleDao();
+		ArrayList<Article> articles = dao.getArticles();
+		
+		String action = request.getParameter("action");
+		
+		
+		//jsp에 articles넘기기
+		//1.request객체에 데이터 저장
+		request.setAttribute("myData",articles);//변수와 넘길데이터
+		//2.위에서 저장한 request객체를 이용해서 새로운 jsp 호출요청->목적지 jsp필요
+		String dest = "";
+		if(action.equals("list1")) {
+			dest = "/list.jsp"; //목적지 jsp
+		}else {
+			dest="/gugu.jsp";
+		}
+		
+		//3.요청하기
+		RequestDispatcher rd = request.getRequestDispatcher(dest); //디스패처는 적절한 jsp에다가 요청을 알맞게 전달해주는 객체 서비스 요 
+		rd.forward(request, response); //forward를 쓰면은 request,response를 인자로 넘긴다.
+	}
+
+}
